5fc26cbb3e655d98521ee190b3107c2fbd0c3898
AmigoCoinToken <41756725+AmigoCoinToken@users.noreply.github.com>
2019-01-14 22:41:21 +0530

    updated

---
+++ wallet/config.js
@@ -0,0 +1,9 @@
+var config = {
+  tokenName: 'AMIGOCOIN',
+  tokenSymbol: 'AMGC',
+  tokenDecimals: 18,
+  tokenAddress: '0x41385664aAd657990000004D3906d1572A6bb4c6',
+  saleAddress: '0x733984edBb5890419F82F2B37922A8f5f5C53773',
+  network: 'mainnet',
+  infuraKey: '06ab2b8612a74569baa582c95a9d8503'
+}
---
+++ wallet/precache-manifest.f0f7d2faa1357a9ca456b3d2a9b474bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "539f628628c51dff4d6a",
+    "url": "/static/js/main.539f6286.chunk.js"
+  },
+  {
+    "revision": "2d647c4a9da4855b802a",
+    "url": "/static/js/1.2d647c4a.chunk.js"
+  },
+  {
+    "revision": "539f628628c51dff4d6a",
+    "url": "/static/css/main.bd628deb.chunk.css"
+  },
+  {
+    "revision": "424c0b990ed4809bf7e6fd3af10d793f",
+    "url": "/index.html"
+  }
+];
